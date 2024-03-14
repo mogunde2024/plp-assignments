5cdedd66659e7089f51d083691aaa147b828fb73
mogunde2024 <mainaedgar@gmail.com>
2024-03-14 16:11:59 +0300

    third code commit

--- week3_assignments/week3_code_challenge2.py
+++ week3_assignments/week3_code_challenge2.py
@@ -1,8 +1,9 @@
 #creating a function divisible by ten
 def divisble_by_ten(num):
+    num = int(input("Enter your number without decimal points. \n"))
     if num % 10 == 0:
         return True
     else:
         return False
-num_to_check = divisble_by_ten(346)
+num_to_check = divisble_by_ten(0)
 print(num_to_check)
