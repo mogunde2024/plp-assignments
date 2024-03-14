299bfa6702ffd4b1c7b76a238d9b219f13096454
mogunde2024 <mainaedgar@gmail.com>
2024-03-14 14:59:36 +0300

    wk4_first commit

---
+++ my_libraries/My_Dart_files/Data_types.dart
@@ -0,0 +1,21 @@
+void main(){
+  // Declaring variables
+  int num1 = 100; // whole numbers
+  double num2 = 45.34;
+  num num3 = 65;
+  num num4 = 81.456;
+
+  // sum of the numbers
+  num sum = num1 + num2 + num3 + num4;
+
+  // printing the info
+  print("Num1 is $num1");
+  print("Num1 is $num2");
+  print("Num1 is $num3");
+  print("Num1 is $num4");
+  print("Sum is $sum");
+
+
+
+
+}
---
+++ my_libraries/my_nampy.py
@@ -0,0 +1,2 @@
+import numpy as np
+import Matplotlib as pl
