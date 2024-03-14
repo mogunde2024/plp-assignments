812b1a94798a43d601516336e9ac28016740dcf9
mogunde2024 <mainaedgar@gmail.com>
2024-03-14 16:06:30 +0300

    second code commit

--- week3_assignments/week3_code_challenge.py
+++ week3_assignments/week3_code_challenge.py
@@ -1,5 +1,8 @@
 #creating a function with inputs
 def large_power(base, exponent):
+  base = int(input("Enter your first number as a base. \n"))
+  exponent = float(input("Enter your second number as an exponent to the base number. \n"))
+  
   #creating a variable to store the result
   result = base ** exponent
   #creating a if statement to check if the result is greater than 5000
@@ -10,5 +13,5 @@ def large_power(base, exponent):
     #returning the result
     return False
 
-check_number = large_power(2, 7)
+check_number = large_power(0, 0)
 print(check_number)
