1b140b467027482786417b9bd1356a35c5aa26c2
mogunde2024 <mainaedgar@gmail.com>
2024-03-18 21:11:55 +0300

    docstring message

--- week3_assignments/Hackathon_questions/fibonacci_func.py
+++ week3_assignments/Hackathon_questions/fibonacci_func.py
@@ -1,5 +1,15 @@
 #creating Functions & Fibonacci Sequence
-def fibonacci(n):
+def fibonacci(n): 
+    """
+  This function generates the Fibonacci sequence up to a specified term n using iteration.
+
+  Args:
+      n: The number of terms in the Fibonacci sequence.
+
+  Returns:
+      A list containing the Fibonacci sequence up to n terms.
+  """
+    
     sequence = [0, 1]
     #using for loop to generate the fibonacci sequence and appending to the list
     for i in range(2, n):
